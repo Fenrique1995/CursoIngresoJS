c913a0dfa0042529e8a53199691cece9bfa1728e
unknown <fenrique310@gmail.com>
2023-01-31 19:20:49 -0300

    Ejercicio 7 completo

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -2,24 +2,43 @@
 Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resulto por medio de "ALERT"
-ej.: "la Resta es 750"*/ 
-function sumar()
-{	
-	alert("ok");	
+ej.: "la Resta es 750"
+Enrique Santiago Fernandez
+*/
+function sumar() {
+	let numeroUno = parseInt(document.getElementById("txtIdNumeroUno").value);
+	let numeroDos = parseInt(document.getElementById("txtIdNumeroDos").value);
+	let resultado;
+	resultado = numeroUno + numeroDos
+	alert("Resultado de la suma: "+resultado);
 }
 
-function restar()
-{
-	
+function restar() {
+	let numeroUno = parseInt(document.getElementById("txtIdNumeroUno").value);
+	let numeroDos = parseInt(document.getElementById("txtIdNumeroDos").value);
+	let resultado;
+	resultado = numeroUno - numeroDos
+	alert("Resultado de la resta: "+resultado);
 }
 
-function multiplicar()
-{ 
-	
+function multiplicar() {
+	let numeroUno = parseInt(document.getElementById("txtIdNumeroUno").value);
+	let numeroDos = parseInt(document.getElementById("txtIdNumeroDos").value);
+	let resultado;
+	resultado = numeroUno * numeroDos
+	alert("Resultado de la multiplicacion: "+resultado);
 }
 
-function dividir()
-{
-	
+function dividir() {
+	let numeroUno = parseInt(document.getElementById("txtIdNumeroUno").value);
+	let numeroDos = parseInt(document.getElementById("txtIdNumeroDos").value);
+	let resultado;
+	if (numeroDos === 0) {
+		alert("ERROR");
+	} else {
+		resultado = numeroUno / numeroDos
+		alert("Resultado de la division: "+resultado);
+	}
+
 }
 
