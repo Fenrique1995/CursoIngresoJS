e3468211253c883cb5f1522bc7d26a8c395d1174
unknown <fenrique310@gmail.com>
2023-02-02 18:07:00 -0300

    1 Ferrete Facturación (E/S) completo

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -2,16 +2,31 @@
 A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
+Enriquue Santiago Fernandez
 */
-function Sumar () 
-{
-	
+function Sumar() {
+    let precioUno, precioDos, precioTres, suma;
+    precioUno = parseFloat(document.getElementById("txtIdPrecioUno").value);
+    precioDos = parseFloat(document.getElementById("txtIdPrecioDos").value);
+    precioTres = parseFloat(document.getElementById("txtIdPrecioTres").value);
+    suma = precioUno + precioDos + precioTres;
+    alert("La suma de los precios es: " + suma);
 }
-function Promedio () 
-{
-	
+function Promedio() {
+    let precioUno, precioDos, precioTres, promedio;
+    precioUno = parseFloat(document.getElementById("txtIdPrecioUno").value);
+    precioDos = parseFloat(document.getElementById("txtIdPrecioDos").value);
+    precioTres = parseFloat(document.getElementById("txtIdPrecioTres").value);
+    promedio = (precioUno + precioDos + precioTres)/3;
+    alert("El promedio de estos precios es: "+promedio);
 }
-function PrecioFinal () 
-{
-	
+function PrecioFinal() {
+    let precioUno, precioDos, precioTres, suma, iva, precioFinal;
+    precioUno = parseFloat(document.getElementById("txtIdPrecioUno").value);
+    precioDos = parseFloat(document.getElementById("txtIdPrecioDos").value);
+    precioTres = parseFloat(document.getElementById("txtIdPrecioTres").value);
+    suma = precioUno + precioDos + precioTres;
+    iva = suma * 0.21;
+    precioFinal = suma + iva;
+    alert("El precio final seria: "+precioFinal);
 }
