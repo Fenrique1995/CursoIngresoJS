6ce4b3515de546b3df71e9e9d071c537e78a5c1e
unknown <fenrique310@gmail.com>
2023-02-06 15:48:34 -0300

    IF ejercicio 7 completo

--- 2-InstruccionIf/jsInstruccionIF-07.js
+++ 2-InstruccionIf/jsInstruccionIF-07.js
@@ -1,8 +1,17 @@
-function mostrar()
-{
+/*
+Al ingresar una edad menor a 18 años y un estado civil distinto a "Soltero", mostrar el siguiente mensaje: 'Es muy pequeño para NO ser soltero.'
+Enrique Santiago Fernandez
+*/
+function mostrar() {
 	//tomo la edad  
-	alert("ok");
-	
+	let edad, estado;
+	edad = parseInt(document.getElementById("txtIdEdad").value);
+	estado = document.getElementById("estadoCivil").value;
+	if (edad < 18) {
+		if (estado !== "Soltero") {
+			alert("Es muy pequeño para NO ser soltero.");
+		}
+	}
 
 
 }//FIN DE LA FUNCIÓN
