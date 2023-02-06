118ce6886ef9abe85edb51db159b3c38d937fa9a
unknown <fenrique310@gmail.com>
2023-02-06 17:03:32 -0300

    IF ejercicio 10 completo

--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,6 +1,21 @@
-function mostrar()
-{
+/*
+Al presionar el Botón, asignar una nota RANDOM al examen y mostrar:
+"EXCELENTE" para notas igual a 9 o 10,
+"APROBÓ" para notas mayores a 4,
+"Vamos, la proxima se puede" para notas menores a 4
+Enrique Santiago Fernandez
+*/
+function mostrar() {
 	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");
+	let nota = 0;
+	nota = parseInt(Math.random() * 10 + 1);
+
+	if (nota >= 9) {
+		alert(nota+" EXCELENTE");
+	} else if (nota >= 4) {
+		alert(nota+" APROBÓ");
+	} else if (nota < 4) {
+		alert(nota+" Vamos, la proxima se puede");
+	}
 
 }//FIN DE LA FUNCIÓN
