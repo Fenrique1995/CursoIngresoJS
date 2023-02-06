e4d0da23628ccad16f4fe529a929bab003728841
unknown <fenrique310@gmail.com>
2023-02-06 15:31:00 -0300

    IF ejercicio 6

--- 2-InstruccionIf/jsInstruccionIF-06.js
+++ 2-InstruccionIf/jsInstruccionIF-06.js
@@ -1,8 +1,19 @@
-function mostrar()
-{
+/*
+Al ingresar una edad debemos informar si la persona es mayor de edad (mas de 18 años) o adolescente (entre 13 y 17 años) o niño (menor a 13 años).
+Enrique Santiago Fernandez
+*/
+function mostrar() {
 	//tomo la edad  
-	alert("ok");
-
-
-
+	let edad;
+	edad = parseInt(document.getElementById("txtIdEdad").value);
+	if (edad >= 18) {
+		alert("Es mayor de edad");
+	} else if (edad < 18) {
+		if (edad > 12) {
+			alert("Es adolescente");
+		}
+		if (edad <= 12) {
+			alert("Es niño");
+		}
+	}
 }//FIN DE LA FUNCIÓN
