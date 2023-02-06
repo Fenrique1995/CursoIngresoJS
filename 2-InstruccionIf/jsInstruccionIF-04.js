d0db7072f10317c58c4b2ef798ac8e7c8817376e
unknown <fenrique310@gmail.com>
2023-02-06 16:02:25 -0300

    Correccion del ejercicio 4 de IF y ejercicio 8 de IF

--- 2-InstruccionIf/jsInstruccionIF-04.js
+++ 2-InstruccionIf/jsInstruccionIF-04.js
@@ -6,16 +6,7 @@ function mostrar() {
 	//tomo la edad  
 	let edad;
 	edad = parseInt(document.getElementById("txtIdEdad").value);
-	if (edad >= 18) {
-		alert("Es mayor de edad");
-	} else if (edad < 18) {
-		if (edad > 12) {
+		if (edad > 12 && edad < 18) {
 			alert("Es adolescente");
 		}
-		if (edad <= 12) {
-			alert("Es menor");
-		}
-	}
-
-
 }//FIN DE LA FUNCIÓN
