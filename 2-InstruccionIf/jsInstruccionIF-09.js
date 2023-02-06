fa139d75ee5e5b891c70592c0d2bd43811dd97d8
unknown <fenrique310@gmail.com>
2023-02-06 16:38:54 -0300

    Ejercicio 9 completo

--- 2-InstruccionIf/jsInstruccionIF-09.js
+++ 2-InstruccionIf/jsInstruccionIF-09.js
@@ -1,6 +1,13 @@
+/*
+Al presionar el Botón, mostrar un número Random del 1 al 10 inclusive
+Enrique Santiago Fernandez
+*/
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");	
+	let random = 0;
+	random = parseInt(Math.random()*10+1);
+	
 
+	alert("EL NUMERO RANDOM ES: "+random);
 }//FIN DE LA FUNCIÓN
