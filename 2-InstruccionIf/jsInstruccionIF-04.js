390bf40c26c014ca01ce382a6c45f7bb1199fba9
unknown <fenrique310@gmail.com>
2023-02-06 15:17:01 -0300

    IF ejercicio 4 completo

--- 2-InstruccionIf/jsInstruccionIF-04.js
+++ 2-InstruccionIf/jsInstruccionIF-04.js
@@ -1,7 +1,21 @@
-function mostrar()
-{
+/*
+Al ingresar una edad debemos informar si la persona es adolescente, edad entre 13 y 17 años (inclusive) .
+Enrique Santiago Fernandez
+*/
+function mostrar() {
 	//tomo la edad  
-	alert("ok");
+	let edad;
+	edad = parseInt(document.getElementById("txtIdEdad").value);
+	if (edad >= 18) {
+		alert("Es mayor de edad");
+	} else if (edad < 18) {
+		if (edad > 12) {
+			alert("Es adolescente");
+		}
+		if (edad <= 12) {
+			alert("Es menor");
+		}
+	}
 
 
 }//FIN DE LA FUNCIÓN
