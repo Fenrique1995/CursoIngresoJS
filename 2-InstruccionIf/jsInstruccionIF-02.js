0ec5cdaf4121cb9d7b2cbd605d2781410f9fae51
unknown <fenrique310@gmail.com>
2023-02-06 14:48:49 -0300

    IF ejercicio 2 completo

--- 2-InstruccionIf/jsInstruccionIF-02.js
+++ 2-InstruccionIf/jsInstruccionIF-02.js
@@ -1,7 +1,15 @@
+/*
+Al ingresar una edad debemos informar solo si la persona es mayor de edad
+Enrique Santiago Fernandez
+*/
 function mostrar()
 {
 	//tomo la edad  
-	alert("ok");
+	let edad;
+	edad = parseInt(document.getElementById("txtIdEdad").value);
+	if (edad >= 18) {
+		alert("Es mayor de edad");
+	}
 	
 
 }//FIN DE LA FUNCIÓN
