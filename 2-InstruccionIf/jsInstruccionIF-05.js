c0db1badd4a7e9190911c8cd7ebc897c14ff208c
unknown <fenrique310@gmail.com>
2023-02-06 15:25:58 -0300

    IF ejercicio 5 completo

--- 2-InstruccionIf/jsInstruccionIF-05.js
+++ 2-InstruccionIf/jsInstruccionIF-05.js
@@ -1,6 +1,16 @@
-function mostrar()
-{
+/*
+Al ingresar una edad solo debemos informar si la persona NO es adolescente.
+Enrique Santiago Fernandez
+*/
+function mostrar() {
 	//tomo la edad  
-	alert("ok");
+	let edad;
+	edad = parseInt(document.getElementById("txtIdEdad").value);
+
+	if (edad <= 12) {
+		alert("Esta persona no es adolescente");
+	} else if (edad >= 18) {
+		alert("Esta persona no es adolescente");
+	}
 
 }//FIN DE LA FUNCIÓN
