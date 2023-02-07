77bae1ab61a0e520a687e2829fa14264c2d01d81
unknown <fenrique310@gmail.com>
2023-02-07 15:57:39 -0300

    challenge

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -2,9 +2,22 @@
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"
 Enrique Santiago Fernandez*/
-function mostrar()
-{
-	alert("Hola Mundo!!!");
-	alert("Esto funciona de maravilla");
+function mostrar() {
+	//alert("Hola Mundo!!!");
+	//alert("Esto funciona de maravilla");
+	let a, b, c;
+	a = parseInt(Math.random() * 10 + 1);
+	b = parseInt(Math.random() * 10 + 1);
+	c = parseInt(Math.random() * 10 + 1);
+	if (a < b && a < c) {
+		alert("A es el menor: " + a);
+	} else if (b < a && b < c) {
+		alert("B es el menor: " + b);
+	} else if (c < a && c < b) {
+		alert("C es el menor: " + c);
+	}
+	let ejemplo = [5, 8, 2, 4, 3, 6, 7];
+	ejemplo.sort();
+	alert(ejemplo[0]);
 }
 
