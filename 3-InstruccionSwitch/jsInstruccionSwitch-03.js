ccddce38673e3eddf0a51c9b5a8bdeffc1f95eb1
unknown <fenrique310@gmail.com>
2023-02-13 15:17:42 -0300

    ejercicio 3 de switch

--- 3-InstruccionSwitch/jsInstruccionSwitch-03.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-03.js
@@ -1,10 +1,25 @@
-function mostrar()
-{
+/*
+al seleccionar un mes informar.
+si es Febrero: " Este mes no tiene más de 29 días."
+si NO es Febrero: "Este mes tiene 30 o más días"
+Enrique Santiago Fernandez
+*/
+function mostrar() {
 	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
-	
-	
+	let mesDelAño;
+	mesDelAño = document.getElementById("txtIdMes").value;
+
+	switch (mesDelAño) {
+		case "Febrero":
+			alert(" Este mes no tiene más de 29 días.")
+			break;
+		default:
+			alert("Este mes tiene 30 o más días")
+			break;
+	}
+
+
+
 
 
 }//FIN DE LA FUNCIÓN
