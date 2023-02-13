2c278772f1fc3c5f70da99f3d868998fc941f252
unknown <fenrique310@gmail.com>
2023-02-13 15:44:23 -0300

    ejercicio 5 de switch

--- 3-InstruccionSwitch/jsInstruccionSwitch-05.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-05.js
@@ -1,10 +1,24 @@
-function mostrar()
-{
+/*
+Enunciado:
+Al ingresar una hora, informar:
+si está entre las 7 y las 11 : "Es de mañana.".
+Enrique Santiago Fernandez
+*/
+function mostrar() {
 	//tomo la hora
-	var horaDelDia =txtIdHora.value;
-	alert(horaDelDia);
-	
-	
+	let horaDelDia;
+	horaDelDia = parseInt(document.getElementById("txtIdHora").value);
+	switch (horaDelDia) {
+		case horaDelDia:
+		 	if (horaDelDia > 6 && horaDelDia < 12) {
+				alert("Es de mañana.");
+			}
+			break;
+		default:
+			break;
+	}
+
+
 
 
 
