255a1df164bae6c0e69d948582b676a5c1985fb8
unknown <fenrique310@gmail.com>
2023-02-13 16:27:22 -0300

    ejercicio 7 de switch

--- 3-InstruccionSwitch/jsInstruccionSwitch-07.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-07.js
@@ -1,6 +1,27 @@
-function mostrar()
-{
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+/*
+Al selecionar un destino , indicar el punto cardinal de nuestro pais en donde se encuentra Norte, Sur, Este u Oeste
+Enrique Santiago Fernandez
+*/
+function mostrar() {
+	let destinoIngresado;
+	destinoIngresado = document.getElementById("txtIdDestino").value;
+	switch (destinoIngresado) {
+
+		case "Cataratas":
+			alert("Este destino se encuentra en el Norte de nuestro pais");
+			break;
+		case "Bariloche":
+			alert("Este destino se encuentra en el Oeste de nuestro pais");
+			break;
+		case "Mar del plata":
+			alert("Este destino se encuentra en el Este de nuestro pais");
+			break;
+		case "Ushuaia":
+			alert("Este destino se encuentra en el Sur de nuestro pais");
+			break;
+
+		default:
+			break;
+	}
 
 }//FIN DE LA FUNCIÓN
