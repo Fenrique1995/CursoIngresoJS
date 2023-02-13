dbfe29beec4456037222cad7d7b705fab3d4a933
unknown <fenrique310@gmail.com>
2023-02-13 15:28:41 -0300

    ejercicio 4 de switch

--- 3-InstruccionSwitch/jsInstruccionSwitch-04.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-04.js
@@ -1,10 +1,30 @@
-function mostrar()
-{
+/*
+al seleccionar un mes informar.
+si tiene 28 días.
+si tiene 30 días.
+si tiene 31 días.
+Enrique Santiago Fernandez
+*/
+function mostrar() {
 	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
-	
-	
+	let mesDelAño;
+	mesDelAño = document.getElementById("txtIdMes").value;
+	switch (mesDelAño) {
+		case "Febrero":
+			alert("Este mes tiene 28 días o 29 en año bisiesto");
+			break;
+		case "Abril":
+		case "Junio":
+		case "Septiembre":
+		case "Noviembre":
+			alert("Este mes tiene 30 dias");
+			break;
+		default:
+			alert("Este mes tiene 31 dias");
+			break;
+	}
+
+
 
 
 
