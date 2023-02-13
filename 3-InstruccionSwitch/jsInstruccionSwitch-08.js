f7b355b412b8995f6609ebd6f400f782c331916b
unknown <fenrique310@gmail.com>
2023-02-13 16:31:15 -0300

    ejercicio 8 de switch

--- 3-InstruccionSwitch/jsInstruccionSwitch-08.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-08.js
@@ -1,6 +1,22 @@
-function mostrar()
-{
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+/*
+Al seleccionar un destino informar si hace FRIO o CALOR en ese destino
+Enrique Santiago Fernandez
+*/
+function mostrar() {
+	let destinoIngresado;
+	destinoIngresado = document.getElementById("txtIdDestino").value;
+	switch (destinoIngresado) {
+		case "Cataratas":
+		case "Mar del plata":
+			alert("En este destino hace calor");
+			break;
+		case "Bariloche":
+		case "Ushuaia":
+			alert("En este destino hace frio");
+			break;
+
+		default:
+			break;
+	}
 
 }//FIN DE LA FUNCIÓN
