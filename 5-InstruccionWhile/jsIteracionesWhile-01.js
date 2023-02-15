d583939c393ed5a5e43d7f039b9f5f51c9ae3ef0
unknown <fenrique310@gmail.com>
2023-02-15 00:02:53 -0300

    ejercicio 1 de while con mi nombre

--- 5-InstruccionWhile/jsIteracionesWhile-01.js
+++ 5-InstruccionWhile/jsIteracionesWhile-01.js
@@ -1,6 +1,8 @@
 /*
 al presionar el botón mostrar 10 repeticiones 
-con números ASCENDENTE, desde el 1 al 10.*/
+con números ASCENDENTE, desde el 1 al 10.
+Enrique Santiago Fernandez
+*/
 function mostrar()
 {
 	let num = 0;
