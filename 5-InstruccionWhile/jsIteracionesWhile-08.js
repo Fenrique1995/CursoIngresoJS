8c383aa646291cca7fbfeb43cd523913e6d75193
unknown <fenrique310@gmail.com>
2023-02-16 16:37:40 -0300

    ejercicio 8 correccion en la multiplicacion de negativos e identacion

--- 5-InstruccionWhile/jsIteracionesWhile-08.js
+++ 5-InstruccionWhile/jsIteracionesWhile-08.js
@@ -3,31 +3,27 @@ Al presionar el botón pedir  números  hasta que el usuario quiera,
 sumar los que son positivos y multiplicar los negativos.
 Enrique Santiago Fernandez
 */
-function mostrar()
-{
+function mostrar() {
 	let num;
 	let answer;
 	let sumPositives;
 	let plusNegatives;
-	let counter;
-	counter=0;
-	sumPositives=0;
-	plusNegatives=0;
-	answer='si';
+	sumPositives = 0;
+	plusNegatives = 1;
+	answer = 'si';
 
 	while (answer != "no") {
-		counter++;
 		num = parseInt(prompt("Ingrese un numero:"));
 		if (num < 0) {
-			plusNegatives += num;
-		}else if (num > 0) {
+			plusNegatives *= num;
+		} else if (num > 0) {
 			sumPositives += num
 		}
 		answer = prompt("Desea continuar: ");
 	}
 
 
-	document.getElementById("txtIdSuma").value=sumPositives;
-	document.getElementById("txtIdProducto").value=plusNegatives*= counter;
+	document.getElementById("txtIdSuma").value = sumPositives;
+	document.getElementById("txtIdProducto").value = plusNegatives;
 
 }//FIN DE LA FUNCIÓN
