30f96237791bba444ddb24bcc6107c55437eb450
unknown <fenrique310@gmail.com>
2023-02-16 17:18:44 -0300

    ejercicio 9 de while

--- 5-InstruccionWhile/jsIteracionesWhile-09.js
+++ 5-InstruccionWhile/jsIteracionesWhile-09.js
@@ -1,21 +1,35 @@
 /*
 Al presionar el botón pedir  números  hasta que el usuario quiera,
-mostrar el número máximo y el número mínimo ingresado.*/
-function mostrar()
-{	// declarar variables
-	var banderaDelPrimero;
-	var numeroIngresado;
-	var numeroMaximo;
-	var numeroMinimo;
-	var respuesta;
+mostrar el número máximo y el número mínimo ingresado.
+Enrique Santiago Fernandez
+*/
+function mostrar() {	// declarar variables
+	let banderaDelPrimero;
+	let numeroIngresado;
+	let numeroMaximo = 0;
+	let numeroMinimo = 0;
+	let respuesta;
 	//iniciar variables
-	banderaDelPrimero="es el primero";
-	respuesta='si';
-	while(respuesta=="si")
-	{
+	banderaDelPrimero = 0;
+	respuesta = 'si';
+	while (respuesta == "si") {
+		banderaDelPrimero++;
+		numeroIngresado = prompt("Ingrese un numero: ");
+		respuesta = prompt("desea continuar?");
+
+		if (banderaDelPrimero == 1) {
+			numeroMaximo = numeroIngresado;
+			numeroMinimo = numeroIngresado;
+		}	
 		
-		respuesta=prompt("desea continuar?");
+		if (banderaDelPrimero > 1 && numeroIngresado > numeroMaximo) {
+			numeroMaximo = numeroIngresado;
+		} 
+
+		if (banderaDelPrimero > 1 && numeroIngresado < numeroMinimo) {
+			numeroMinimo = numeroIngresado;
+		}
 	}
-	txtIdMaximo.value=numeroMaximo;
-	txtIdMinimmo.value=numeroMinimo;
+	document.getElementById("txtIdMaximo").value = numeroMaximo;
+	document.getElementById("txtIdMinimo").value = numeroMinimo;
 }//FIN DE LA FUNCIÓN
