b857235f50b43af2f9764047f3ca33f3c893d43b
unknown <fenrique310@gmail.com>
2023-02-16 14:39:02 -0300

    ejercicio 3 de while e identado del ejercicio 2

--- 5-InstruccionWhile/jsIteracionesWhile-02.js
+++ 5-InstruccionWhile/jsIteracionesWhile-02.js
@@ -3,10 +3,9 @@ al presionar el botón mostrar 10 repeticiones
 con números DESCENDENTES, desde el 10 al 1.
 Enrique Santiago Fernandez
 */
-function mostrar()
-{
+function mostrar() {
 	let contador;
-	contador=10; 
+	contador = 10;
 	while (contador > 0) {
 		alert(contador);
 		contador--;
--- 5-InstruccionWhile/jsIteracionesWhile-03.js
+++ 5-InstruccionWhile/jsIteracionesWhile-03.js
@@ -1,8 +1,10 @@
 /*
-al presionar el botón pedir la CLAVE (ayuda: es utn750)*/
-function mostrar()
-{
-	var claveIngresada;
-	claveIngresada = prompt("ingrese el número clave.");
-	
+al presionar el botón pedir la CLAVE (ayuda: es utn750)
+Enrique Santiago Fernandez
+*/
+function mostrar() {
+	let claveIngresada;
+	while (claveIngresada !== "utn750") {
+		claveIngresada = prompt("ingrese el número clave.");
+	}
 }//FIN DE LA FUNCIÓN
