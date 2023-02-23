187308a39c09f90f9e74680b771b7e8a55b097cd
unknown <fenrique310@gmail.com>
2023-02-23 15:32:58 -0300

    correccion en el ejercicio 9 de while

--- 5-InstruccionWhile/jsIteracionesWhile-09.js
+++ 5-InstruccionWhile/jsIteracionesWhile-09.js
@@ -6,29 +6,29 @@ Enrique Santiago Fernandez
 function mostrar() {	// declarar variables
 	let banderaDelPrimero;
 	let numeroIngresado;
-	let numeroMaximo = 0;
-	let numeroMinimo = 0;
+	let numeroMaximo;
+	let numeroMinimo;
 	let respuesta;
 	//iniciar variables
 	banderaDelPrimero = 0;
 	respuesta = 'si';
 	while (respuesta == "si") {
 		banderaDelPrimero++;
-		numeroIngresado = prompt("Ingrese un numero: ");
-		respuesta = prompt("desea continuar?");
+		numeroIngresado = parseInt(prompt("Ingrese un numero: "));
 
-		if (banderaDelPrimero == 1) {
+		if(banderaDelPrimero == 1)       {
 			numeroMaximo = numeroIngresado;
 			numeroMinimo = numeroIngresado;
-		}	
-		
+		}
+
 		if (banderaDelPrimero > 1 && numeroIngresado > numeroMaximo) {
 			numeroMaximo = numeroIngresado;
-		} 
+		}
 
 		if (banderaDelPrimero > 1 && numeroIngresado < numeroMinimo) {
 			numeroMinimo = numeroIngresado;
 		}
+		respuesta = prompt("desea continuar?");
 	}
 	document.getElementById("txtIdMaximo").value = numeroMaximo;
 	document.getElementById("txtIdMinimo").value = numeroMinimo;
