4eb26f9323ccacf2c8389b6d3d6020f4d53b86ca
unknown <fenrique310@gmail.com>
2023-02-27 15:04:59 -0300

    ejercicio 2 de For

--- 6-InstruccionFor/jsIteracionesFor-02.js
+++ 6-InstruccionFor/jsIteracionesFor-02.js
@@ -1,5 +1,10 @@
-function mostrar()
-{
+/*
+al presionar el botón mostrar 10 repeticiones con números , desde el 10 al 1.
+Enrique Santiago Fernandez
+*/
+function mostrar() {
 
-	alert("ok");
+	for (let i= 10; i >= 1; i--) {
+		alert(i);
+	}
 }
